62ecde7828b87f487b18965ce3d794fc9570178e
Guitar <wachsirin.sara@bumail.net>
2023-01-24 21:25:13 +0700

    2023 01 24 Raycast (Review content)
    Fundamentals of Raycast in Unity. (Review content)

---
+++ 2023 01 24 Raycast/Assets/Scripts/RC2.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class RC2 : MonoBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 origin = transform.position;
+        // Direction in short style
+        Vector3 direction = transform.right;
+        
+        // Raycast(1)
+        Debug.DrawRay(origin, direction * 3, Color.green);
+
+        // Raycast(2)
+        Ray ray = new Ray(origin, direction);
+        // If Raycast(2) collide with <any object>
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 3))
+        {
+            // Change <any object> to yellow
+            hitInfo.collider.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+}
